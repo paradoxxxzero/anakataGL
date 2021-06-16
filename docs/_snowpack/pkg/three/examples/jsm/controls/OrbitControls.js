7617b421a01f323627532839841d8d13254d9015
Florian Mounier <florian.mounier@kozea.fr>
2021-06-16 16:52:28 +0200

    Build

--- docs/_snowpack/pkg/three/examples/jsm/controls/OrbitControls.js
+++ docs/_snowpack/pkg/three/examples/jsm/controls/OrbitControls.js
@@ -1,4 +1,4 @@
-import { w as EventDispatcher, V as Vector3, x as MOUSE, y as TOUCH, Q as Quaternion, z as Spherical, o as Vector2 } from '../../../../common/three.module-774f2111.js';
+import { v as EventDispatcher, V as Vector3, w as MOUSE, x as TOUCH, Q as Quaternion, y as Spherical, z as Vector2 } from '../../../../common/three.module-0c9aba9f.js';
 
 // This set of controls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
